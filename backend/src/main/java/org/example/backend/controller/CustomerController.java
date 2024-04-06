ff7c5de614c3ea83f2a47f90c42038307bd63452
R-Schreyer <roland_schreyer@web.de>
2024-04-06 17:41:40 +0200

    add first test for getCustomeByID and saveNewCustomer

--- backend/src/main/java/org/example/backend/controller/CustomerController.java
+++ backend/src/main/java/org/example/backend/controller/CustomerController.java
@@ -3,6 +3,9 @@ import lombok.RequiredArgsConstructor;
 import org.example.backend.model.Customer;
 import org.example.backend.model.CustomerDTO;
 import org.example.backend.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 @RestController
@@ -18,10 +21,20 @@ public class CustomerController {
     public Customer getCustomerById(@PathVariable String id) {
         return service.getCustomerById(id);
     }
+
+    @Autowired
+    private CustomerService customerService;
+
     @PostMapping
+    public ResponseEntity<Customer> saveNewCustomer(@RequestBody CustomerDTO customerDTO) {
+        Customer newCustomer = customerService.saveCustomer(customerDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newCustomer);
+    }
+    /*@PostMapping
     public Customer saveNewCustomer(@RequestBody CustomerDTO customer){
         return service.saveCustomer(customer);
     }
+    */
     @PutMapping("{id}")
     public Customer updateCustomerById(@PathVariable String id, @RequestBody CustomerDTO customer){
        return service.updateCustomer(id, customer);
--- backend/src/test/java/org/example/backend/controller/CustomerControllerTest.java
+++ backend/src/test/java/org/example/backend/controller/CustomerControllerTest.java
@@ -1,41 +1,37 @@
 package org.example.backend.controller;
-
-import static org.mockito.Mockito.*;
+/*import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.internal.verification.VerificationModeFactory.times;*/
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.example.backend.controller.CustomerController;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.backend.model.Customer;
 import org.example.backend.model.CustomerDTO;
 import org.example.backend.service.CustomerService;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-
 @SpringBootTest
 @AutoConfigureMockMvc
 public class CustomerControllerTest {
-
     @Autowired
     private MockMvc mockMvc;
     @Autowired
     private CustomerService customerService;
-
+    @Autowired
+    private ObjectMapper objectMapper;
+    /*@MockBean
+    private CustomerService service;*/
     @Test
     @DirtiesContext
     public void getAllCustomers() throws Exception {
-        CustomerDTO custemerToSave = new CustomerDTO ("John", "Doe");
-        Customer customerSaved = customerService.saveCustomer(custemerToSave);
+        CustomerDTO customerToSave = new CustomerDTO ("John", "Doe");
+        Customer customerSaved = customerService.saveCustomer(customerToSave);
         mockMvc.perform(get("/api/customers"))
                 .andExpect(status().isOk())
                 .andExpect(content().json("""
@@ -43,6 +39,68 @@ public class CustomerControllerTest {
                         "firstname":  "John",
                         "lastname":  "Doe"}]"""))
                 .andExpect(jsonPath("$[0].id").value(customerSaved.getId()));
-
     }
+    @Test
+    @DirtiesContext
+    public void getCustomerById() throws Exception {
+        CustomerDTO customerToSave = new CustomerDTO ("John", "Doe");
+        Customer customerSaved = customerService.saveCustomer(customerToSave);
+        mockMvc.perform(get("/api/customers/{id}", customerSaved.getId()))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(customerSaved.getId()))
+                .andExpect(jsonPath("$.firstname").value("John"))
+                .andExpect(jsonPath("$.lastname").value("Doe"));
+    }
+    @Test
+    @DirtiesContext
+    public void saveNewCustomer() throws Exception {
+        String customerJson = "{\"firstname\": \"Jane\", \"lastname\": \"Doe\"}";
+        mockMvc.perform(post("/api/customers")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(customerJson))
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.firstname").value("Jane"))
+                .andExpect(jsonPath("$.lastname").value("Doe"));
+    }
+    /*@Test
+    @DirtiesContext
+    void updateCustomerById()  throws Exception {
+        // Given
+        String customerId = "6611637bce485b181c17706f";
+        CustomerDTO customerDTO = new CustomerDTO("John", "Doe");
+
+        // When
+        ResultActions resultActions = mockMvc.perform(put("/api/customers/{id}", customerId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(customerDTO)));
+
+        // Then
+        resultActions.andExpect(status().isOk());
+        // Additional assertions if needed
+
+    }*/
+
+    /*@Test
+    @DirtiesContext
+    void deleteCustomerById() throws Exception {
+        CustomerDTO customerToSave = new CustomerDTO ("John", "Doe");
+        Customer customerSaved = customerService.saveCustomer(customerToSave);
+        String customerId = mockMvc.perform(get("/api/customers/{id}", customerSaved.getId()),
+
+        // Mocking the service method
+        doNothing().when(service).deleteCustomerById(customerId),
+
+        // Performing the delete request
+        mockMvc.perform(delete("/api/customers/{id}" + customerId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()));
+
+        // Verifying that the service method was called
+        verify(service, times(1)).deleteCustomerById(customerId);
+    }*/
+
 }
+
