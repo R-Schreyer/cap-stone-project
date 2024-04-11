a792e5525f98f810e684443d04423f3798fec0f5
R-Schreyer <roland_schreyer@web.de>
2024-04-11 15:44:15 +0200

    fix issues

--- backend/src/test/java/org/example/backend/service/ProductServiceTest.java
+++ backend/src/test/java/org/example/backend/service/ProductServiceTest.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-public class ProductServiceTest {
+class ProductServiceTest {
     ProductRepository productRepository = mock(ProductRepository.class);
     ProductService productService = new ProductService(productRepository);
     @Test
