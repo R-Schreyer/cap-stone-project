30cb911a00c86ed2cc09c720fa0150dc056cab1c
R-Schreyer <roland_schreyer@web.de>
2024-04-13 23:02:15 +0200

    add frontend for order view but direct on site

--- frontend/src/components/Customer/CustomerList.tsx
+++ frontend/src/components/Customer/CustomerList.tsx
@@ -2,17 +2,21 @@ import {Customer} from "../../types/Customer.ts";
 import axios from "axios";
 import React, {useState} from "react";
 import ChangeCustomer from "./ChangeCustomer.tsx";
+import {Order} from "../../types/Order.ts";
+import ViewOrders from "../Order/ViewOrders.tsx";
+
 
 type CustomerListProps = {
     customers: Customer[],
     setCustomers: React.Dispatch<React.SetStateAction<Customer[]>>
     updateCustomer: (id: string, firstname: string, lastname: string, address: string, email: string) => void
+    sendCustomerOrderList: (orderList: Order[]) => void;
 }
 
 export default function CustomerList(props: Readonly<CustomerListProps>) {
     const [editMode, setEditMode] = useState(false)
     const [customer, setCustomer] = useState<Customer>()
-
+    const [customerOrderList, setCustomerOrderList] = useState<Order[]>([])
     function deleteCustomer(id: string) {
         axios.delete("/api/customers/" + id)
             .then(() => {
@@ -33,6 +37,14 @@ export default function CustomerList(props: Readonly<CustomerListProps>) {
     function updateCustomer(id: string, firstname: string, lastname: string, address: string, email: string) {
         props.updateCustomer(id, firstname, lastname, address, email)
         setEditMode(false);
+    }
+
+    function handleViewOrdersClick(customerId: string) {
+        const selectedCustomer = props.customers.find(customer => customer.id === customerId);
+        if (selectedCustomer) {
+            setCustomerOrderList(selectedCustomer.customerOrderList);
+            props.sendCustomerOrderList(selectedCustomer.customerOrderList);
+        }
     }
 
     return (
@@ -56,14 +68,13 @@ export default function CustomerList(props: Readonly<CustomerListProps>) {
                                     setEditMode(true)
                                 }}>Edit
                                 </button>
-                                <button
-                                    onClick={() => console.log("Bestellungen anzeigen für:", customer)}>Bestellungen
-                                </button>
+                                <button onClick={() => handleViewOrdersClick(customer.id)}>View Orders</button>
                             </td>
                         </tr>
                     ))}
                 </table>
                 {editMode && customer && <ChangeCustomer customer={customer} updateCustomer={updateCustomer}/>}
+                {customerOrderList.length > 0 && <ViewOrders customerOrderList={customerOrderList}/>}
             </div>
         </div>
     )
---
+++ frontend/src/components/Order/ViewOrders.tsx
@@ -0,0 +1,26 @@
+import {Order} from "../../types/Order.ts";
+
+interface ViewOrdersProps {
+    customerOrderList: Order[];
+}
+
+function ViewOrders(props: ViewOrdersProps) {
+    return (
+        <div>
+            <h2>Orders</h2>
+            <ul>
+                {props.customerOrderList.map(order => (
+                    <li key={order.id}>
+                        {/* Hier könntest du die Informationen für jede Bestellung anzeigen */}
+                        {/* Zum Beispiel: */}
+                        <div>ID: {order.id}</div>
+                        <div>Price: {order.price}</div>
+                        {/* Weitere Bestellinformationen hier einfügen */}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default ViewOrders;
