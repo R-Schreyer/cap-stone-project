6df7ee30027eb1b269a585b77544f75edbf3a9fa
R-Schreyer <roland_schreyer@web.de>
2024-04-16 21:34:58 +0200

    add feature update products

--- backend/src/test/java/org/example/backend/controller/ProductControllerTest.java
+++ backend/src/test/java/org/example/backend/controller/ProductControllerTest.java
@@ -3,11 +3,15 @@ package org.example.backend.controller;
 import org.example.backend.model.Product;
 import org.example.backend.model.ProductDTO;
 import org.example.backend.service.ProductService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -65,5 +69,34 @@ class ProductControllerTest {
                 .andExpect(jsonPath("$.id").exists())
                 .andExpect(jsonPath("$.productName").value("Water"))
                 .andExpect(jsonPath("$.category").value("Drinks"));
+    }
+
+    @Test
+    @DirtiesContext
+    public void testUpdateProductById() {
+        ProductService productService = Mockito.mock(ProductService.class);
+
+        String productId = "123";
+        BigDecimal pricePerPiece = new BigDecimal("10.0");
+        ProductDTO productDTO = new ProductDTO("Updated Product Name", "Updated Category", pricePerPiece);
+
+        Product updatedProduct = new Product(productId, productDTO.productName(), productDTO.category(), productDTO.pricePerPiece());
+        Mockito.when(productService.updateProduct(Mockito.eq(productId), Mockito.any(ProductDTO.class))).thenReturn(updatedProduct);
+
+        ProductController productController = new ProductController(productService);
+
+        ResponseEntity<Product> responseEntity = productController.updateProductById(productId, productDTO);
+
+        Assertions.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+
+        Assertions.assertNotNull(responseEntity.getBody());
+
+        Product updatedProductResponse = responseEntity.getBody();
+        Assertions.assertEquals(productId, updatedProductResponse.getId());
+        Assertions.assertEquals(productDTO.productName(), updatedProductResponse.getProductName());
+        Assertions.assertEquals(productDTO.category(), updatedProductResponse.getCategory());
+        Assertions.assertEquals(pricePerPiece, updatedProductResponse.getPricePerPiece());
+
+        Mockito.verify(productService, Mockito.times(1)).updateProduct(Mockito.eq(productId), Mockito.eq(productDTO));
     }
 }
